1be548db72d89ec150f2435e6ede76db4d085a45
bhargav5667 <bhargavvshivankar@gmail.com>
2025-04-01 10:44:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import StockSearch from './components/StockSearch';
+
+
+function App() {
+  return (
+    <div className="App">
+      <StockSearch />
+    </div>
+  );
+}
+
+export default App;
